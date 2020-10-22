a8958ec24a27505be8f0a11522d754b02d7b102a
mukhindev <mukhin.dev@gmail.com>
2020-10-22 12:31:34 +0300

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const { celebrate, Joi, CelebrateError } = require('celebrate');
@@ -27,6 +28,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 
 app.use(limiter);
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(requestLogger);
